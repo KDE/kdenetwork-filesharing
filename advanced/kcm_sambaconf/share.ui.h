523ce3cd7afaa1cea8b192a7ee657ea331a2d367
Jan Schäfer <j_schaef@informatik.uni-kl.de>
2002-06-12 23:33:48 +0000

    changed the layout a little bit
    svn path=/trunk/kdenetwork/filesharing/; revision=160644

--- advanced/kcm_sambaconf/share.ui.h
+++ advanced/kcm_sambaconf/share.ui.h
@@ -6,35 +6,6 @@
 ** a constructor, and a destroy() slot in place of a destructor.
 *****************************************************************************/
 
-void KcmShareDlg::edit( shareData * data )
-{
-/*
-	shareName->setText(data->name);
-	share = data;
-	if (data->directory){
-		if (data->name == "[homes]"){
-			homesChk->setChecked(true);
-		}else{
-			pathUrlRq->setURL(data->path);
-			homesChk->setChecked(false);
-		}
-	}else{
-		if (data->name == "[printers]"){
-			printersChk->setChecked(true);
-		}else{
-			queue->setCurrentItem(data->queue);
-			printersChk->setChecked(false);
-		}
-	}
-	directory->setChecked(data->directory);
-	browse->setChecked(data->browse);
-	read->setChecked(data->read);
-	publ->setChecked(data->publ);
-	selectUsers->setChecked(data->users);
-	enableUserSelection->setChecked(data->enableUserSelection);
-*/
-}
-
 void KcmShareDlg::checkValues()
 {
 /*
@@ -63,66 +34,13 @@ void KcmShareDlg::checkValues()
 }
 
 
-void KcmShareDlg::updateShareData()
-{
-/*
-    bool error = false;
-    if (directory->isChecked()){
-	share->special = homes->isChecked();
-	if (share->special){
-	    share->name = "[homes]";
-	    share->path = i18n("Home directories");
-	}else{
-	    share->name = shareName->text();
-	    share->path = path->url();
-	}
-	share->read = read->isChecked();
-    }else{
-	share->special = printers->isChecked();
-	if (share->special){
-	    share->name = "[printers]";
-	    share->queue = i18n("All printers");
-	}else{
-	    share->name = shareName->text();
-	    share->queue = queue->currentText();
-	}
-    }
-    share->comment = comment->text();
-    share->browse = browse->isChecked();
-    share->directory = directory->isChecked();
-    share->publ = publ->isChecked();
-    share->users = selectUsers->isChecked();
-    share->enableUserSelection = selectUsers->isChecked();
-*/
-}
 
 
-
-void KcmShareDlg::checkTypes( bool directory )
-{
-/*
-    bool temp = false;
-    if (directory){	
-	if (!homes->isChecked()){
-	    temp = true;
-	}
-    }else{
-	if (!printers->isChecked()){
-	    temp = true;
-	}
-    }
-    Lbl_shareName->setEnabled(temp);
-    shareName->setEnabled(temp);
-//    Lbl_shareName->setDisabled(homes->isChecked()|printers->isChecked());
-//    shareName->setDisabled(homes->isChecked()|printers->isChecked());
-*/
-}
 
 
 void KcmShareDlg::init()
 {
     directoryPixLbl->setPixmap(DesktopIcon("folder"));
-    printerPixLbl->setPixmap(DesktopIcon("printer1"));
     
 /*
     QListBoxItem* item = 0;
@@ -144,42 +62,6 @@ void KcmShareDlg::init()
 }
 
 
-void KcmShareDlg::delUsers()
-{
-/*
-    QListBoxItem* item = 0;
-    QPtrList<QListBoxItem>* selectedList = new QPtrList<QListBoxItem>;
-    selectedList->setAutoDelete(false);
-    for (int i=0; i < selected->count(); i++){
-	item = selected->item(i);
-	if (item->isSelected()){
-	    selectedList->append(item);
-	}
-    }
-    possible->clearSelection();
-    for (QPtrListIterator<QListBoxItem> it(*selectedList); it.current(); ++it){
-	selected->takeItem(it.current());
-	possible->insertItem(it.current());
-    }
-    delete selectedList;
-*/
-}
-
-
-void KcmShareDlg::createHomes( bool value )
-{
-/*
-    homesExist = value;
-*/
-}
-
-
-void KcmShareDlg::createPrinters( bool value )
-{
-/*
-    printersExist = value;
-*/
-}
 
 
 void KcmShareDlg::trytoAccept()
@@ -209,16 +91,3 @@ void KcmShareDlg::trytoAccept()
 }
 
 
-void KcmShareDlg::checkHomesAndPrinters()
-{
-/*
-    bool homesState = false;
-    bool printersState = false;
-    if (homesExist)
-	homesState=true;
-    if (printersExist)
-	printersState=true;
-    homesGroup->setDisabled(homesState);
-    printersGroup->setDisabled(printersState);
-*/
-}

4143f524afad2c92a6cefff7334ab6b127eed087
Jan Schäfer <j_schaef@informatik.uni-kl.de>
2002-07-11 16:13:12 +0000

    a function to set the current item of a combo box to a specified text
    svn path=/trunk/kdenetwork/filesharing/; revision=166077

---
+++ advanced/kcm_sambaconf/common.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+                          common.h  -  description
+                             -------------------
+    begin                : Tue June 6 2002
+    copyright            : (C) 2002 by Jan Schäfer
+    email                : janschaefer@users.sourceforge.net
+ ***************************************************************************/
+
+/******************************************************************************
+ *                                                                            *
+ *  This file is part of KSambaPlugin.                                          *
+ *                                                                            *
+ *  KSambaPlugin is free software; you can redistribute it and/or modify            *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation; either version 2 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  KSambaPlugin is distributed in the hope that it will be useful,                 *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with KSambaPlugin; if not, write to the Free Software                     *
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
+ *                                                                            *
+ ******************************************************************************/
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <qstring.h>
+#include <qcombobox.h>
+#include <qlistbox.h>
+
+void setComboToString(QComboBox* combo,const QString & s)
+{
+  QListBoxItem* li = combo->listBox()->findItem( s );
+  combo->listBox()->setCurrentItem(li);
+}
+
+#endif

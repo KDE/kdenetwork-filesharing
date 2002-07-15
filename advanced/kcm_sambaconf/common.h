b8295011c0245c35cd770379c619801fa9fd7efe
Jan Schäfer <j_schaef@informatik.uni-kl.de>
2002-07-15 16:25:06 +0000

    moved implemention to common.cpp
    svn path=/trunk/kdenetwork/filesharing/; revision=166810

--- advanced/kcm_sambaconf/common.h
+++ advanced/kcm_sambaconf/common.h
@@ -8,20 +8,20 @@
 
 /******************************************************************************
  *                                                                            *
- *  This file is part of KSambaPlugin.                                          *
+ *  This file is part of KSambaPlugin.                                        *
  *                                                                            *
- *  KSambaPlugin is free software; you can redistribute it and/or modify            *
+ *  KSambaPlugin is free software; you can redistribute it and/or modify      *
  *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation; either version 2 of the License, or         *
  *  (at your option) any later version.                                       *
  *                                                                            *
- *  KSambaPlugin is distributed in the hope that it will be useful,                 *
+ *  KSambaPlugin is distributed in the hope that it will be useful,           *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
  *  GNU General Public License for more details.                              *
  *                                                                            *
  *  You should have received a copy of the GNU General Public License         *
- *  along with KSambaPlugin; if not, write to the Free Software                     *
+ *  along with KSambaPlugin; if not, write to the Free Software               *
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
  *                                                                            *
  ******************************************************************************/
@@ -29,14 +29,9 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <qstring.h>
-#include <qcombobox.h>
-#include <qlistbox.h>
+class QString;
+class QComboBox;
 
-void setComboToString(QComboBox* combo,const QString & s)
-{
-  QListBoxItem* li = combo->listBox()->findItem( s );
-  combo->listBox()->setCurrentItem(li);
-}
+void setComboToString(QComboBox* combo,const QString & s);
 
 #endif

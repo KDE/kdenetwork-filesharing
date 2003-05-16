ea0f67f765177848fc246051cc0a0b7dde2c3751
Jan Schäfer <j_schaef@informatik.uni-kl.de>
2003-05-16 22:49:52 +0000

    new class to configure the location of the smb.conf file
    svn path=/trunk/kdenetwork/filesharing/; revision=226290

---
+++ advanced/kcm_sambaconf/smbconfconfigwidget.cpp
@@ -0,0 +1,87 @@
+/***************************************************************************
+                          smbconfconfigwidget.cpp  -  description
+                             -------------------
+    begin                : Tue May 16 2003
+    copyright            : (C) 2003 by Jan Schäfer
+    email                : janschaefer@users.sourceforge.net
+ ***************************************************************************/
+
+/******************************************************************************
+ *                                                                            *
+ *  This file is part of KSambaPlugin.                                        *
+ *                                                                            *
+ *  KSambaPlugin is free software; you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation; either version 2 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  KSambaPlugin is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with KSambaPlugin; if not, write to the Free Software               *
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
+ *                                                                            *
+ ******************************************************************************/
+
+#include <qfileinfo.h> 
+#include <qlayout.h>
+#include <qpushbutton.h>
+#include <qlabel.h>
+ 
+#include <kapp.h> 
+#include <kfiledialog.h>
+#include <klocale.h>
+#include <kmessagebox.h>
+ 
+
+#include "smbconfconfigwidget.h"
+
+SmbConfConfigWidget::SmbConfConfigWidget(QWidget* parent) 
+  : QWidget(parent,"configWidget")
+{  
+
+  QVBoxLayout *layout = new QVBoxLayout(this,5);
+
+  QLabel *lbl = new QLabel(i18n("<p>The SAMBA configuration file <strong>'smb.conf'</strong>" \
+                                " could not be found!</p>" \
+                                "Make sure you have SAMBA installed.\n\n"), this);
+
+  QHBoxLayout *hbox = new QHBoxLayout(this);
+  QPushButton *btn = new QPushButton(i18n("Specify Location"), this);
+  connect(btn, SIGNAL(pressed()), this, SLOT( btnPressed()));
+
+  btn->setDefault(false);
+  btn->setAutoDefault(false);
+
+  hbox->addStretch();
+  hbox->addWidget(btn);
+
+  layout->addWidget(lbl);
+  layout->addLayout(hbox);
+  layout->addStretch();
+}
+
+void SmbConfConfigWidget::btnPressed() {
+  QString smbConf = KFileDialog::getOpenFileName("/",
+            "smb.conf|Samba conf. File\n"
+            "*|All Files",0,i18n("Get smb.conf Location"));
+
+  if ( ! QFileInfo(smbConf).isReadable() ) {
+    KMessageBox::sorry(this,i18n("<qt>The file <i>%1</i> could not be read !</qt>").arg(smbConf),i18n("File could not be read"));
+    return;
+  }
+
+  KConfig config("ksambaplugin");
+  
+  config.setGroup("KSambaKonqiPlugin");
+  config.writeEntry("smb.conf",smbConf);
+  config.sync();
+  
+  emit smbConfChoosed(smbConf);
+
+}
+
+#include "smbconfconfigwidget.moc"
---
+++ advanced/kcm_sambaconf/smbconfconfigwidget.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+                          smbconfconfigwidget.h  -  description
+                             -------------------
+    begin                : Tue May 16 2003
+    copyright            : (C) 2003 by Jan Schäfer
+    email                : janschaefer@users.sourceforge.net
+ ***************************************************************************/
+
+/******************************************************************************
+ *                                                                            *
+ *  This file is part of KSambaPlugin.                                        *
+ *                                                                            *
+ *  KSambaPlugin is free software; you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation; either version 2 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  KSambaPlugin is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with KSambaPlugin; if not, write to the Free Software               *
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
+ *                                                                            *
+ ******************************************************************************/
+
+#ifndef SMBCONFCONFIGWIDGET_H
+#define SMBCONFCONFIGWIDGET_H
+
+#include <qwidget.h>
+
+class SmbConfConfigWidget : public QWidget {
+Q_OBJECT
+public:
+  SmbConfConfigWidget(QWidget*);
+  
+protected slots:
+  void btnPressed();  
+signals:
+  void smbConfChoosed(const QString &);  
+};
+
+
+#endif

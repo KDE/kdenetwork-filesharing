cc0cbe224f3e77ed66529f95b268d9ab51034591
Jan Schäfer <j_schaef@informatik.uni-kl.de>
2003-05-17 09:15:04 +0000

    new class to check the permissions of users
    svn path=/trunk/kdenetwork/filesharing/; revision=226347

---
+++ advanced/kcm_sambaconf/linuxpermissionchecker.cpp
@@ -0,0 +1,202 @@
+/***************************************************************************
+    begin                : Tue May 17 2003
+    copyright            : (C) 2003 by Jan Schäfer
+    email                : janschaefer@users.sourceforge.net
+ ***************************************************************************/
+
+/******************************************************************************
+ *                                                                            *
+ *  This file is part of KSambaPlugin.                                        *
+ *                                                                            *
+ *  KSambaPlugin is free software; you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation; either version 2 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  KSambaPlugin is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with KSambaPlugin; if not, write to the Free Software               *
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
+ *                                                                            *
+ ******************************************************************************/
+#include <qregexp.h> 
+#include <qstringlist.h> 
+
+#include <kmessagebox.h>
+#include <klocale.h>
+#include <kdebug.h>
+
+#include "passwd.h"
+#include "sambashare.h"  
+#include "linuxpermissionchecker.h"
+
+LinuxPermissionChecker::LinuxPermissionChecker(SambaShare* share,QWidget* parent = 0L)
+{
+  m_sambaShare = share;
+  m_parent = parent;
+
+  if (!share) {
+    kdWarning() << "WARNING: LinuxPermissionChecker: share is null !" << endl;
+    return;        
+  }
+  
+  m_fi = QFileInfo(share->getValue("path"));
+  
+  if ( ! m_fi.exists()) {
+    kdDebug() << "LinuxPermissionChecker: path does not exists !" << endl;
+  }
+
+}
+
+
+LinuxPermissionChecker::~LinuxPermissionChecker()
+{
+}
+
+bool LinuxPermissionChecker::checkAllPermissions() {
+  if (! m_sambaShare )
+     return true;
+     
+  if ( ! m_fi.exists())
+     return true;   
+
+  if (! checkPublicPermissions())
+     return false;     
+     
+  if (! checkAllUserPermissions())
+     return false;
+     
+  return true;          
+}
+    
+bool LinuxPermissionChecker::checkAllUserPermissions() {
+  if (! m_sambaShare )
+     return true;
+
+  if ( ! m_fi.exists())
+     return true;   
+       
+  QStringList readList = QStringList::split(QRegExp("[,\\s]+"),m_sambaShare->getValue("read list"));
+  
+  for ( QStringList::Iterator it = readList.begin(); it != readList.end(); ++it )
+  {
+    if (!checkUserReadPermissions(*it))
+       return false;
+  }    
+  
+  QStringList writeList = QStringList::split(QRegExp("[,\\s]+"),m_sambaShare->getValue("write list"));
+  
+  for ( QStringList::Iterator it = writeList.begin(); it != writeList.end(); ++it )
+  {
+    if (!checkUserWritePermissions(*it))
+       return false;
+  }    
+  
+  return true;     
+}
+
+bool LinuxPermissionChecker::checkPublicPermissions() {
+  if (! m_sambaShare )
+     return true;
+
+
+  bool isPublic = m_sambaShare->getBoolValue("public");
+  if (!isPublic)
+     return true;
+  
+  QString guestAccount = m_sambaShare->getValue("guest account");
+  
+  if ( ! checkUserReadPermissions(guestAccount,false))
+  {
+     if (KMessageBox::Cancel == KMessageBox::warningContinueCancel(
+         0L,i18n(
+           "<qt>You have specified public read access for this directory, but "
+           "the guest account <i>%1</i> doesn't have the needed read permissions !<br>" 
+           "Do you want to continue nevertheless ?</qt>").arg(guestAccount)
+           ,i18n("Warning")
+           ,KStdGuiItem::cont()
+           ,"KSambaPlugin_guestAccountHasNoReadPermissionsWarning"))
+         return false;
+  }
+  
+  
+  if ( ! checkUserWritePermissions(guestAccount,false))
+  {
+     if (KMessageBox::Cancel == KMessageBox::warningContinueCancel(
+         0L,i18n(
+           "<qt>You have specified public write access for this directory, but "
+           "the guest account <i>%1</i> doesn't have the needed write permissions !<br>" 
+           "Do you want to continue nevertheless ?</qt>").arg(guestAccount)
+           ,i18n("Warning")
+           ,KStdGuiItem::cont()
+           ,"KSambaPlugin_guestAccountHasNoWritePermissionsWarning"))
+         return false;
+  }
+  
+  return true;
+}
+    
+bool LinuxPermissionChecker::checkUserPermissions(const QString & user) {
+  if ( ! checkUserReadPermissions(user))
+     return false;  
+  
+  if ( ! checkUserWritePermissions(user))
+     return false;    
+       
+  return true;     
+}
+
+bool LinuxPermissionChecker::checkUserWritePermissions(const QString & user, bool showMessageBox) {
+  if (m_sambaShare->getBoolValue("read only")) {
+  
+    if (! ((m_fi.permission(QFileInfo::WriteOther)) ||
+           (m_fi.permission(QFileInfo::WriteUser) && user == m_fi.owner()) ||
+           (m_fi.permission(QFileInfo::WriteGroup) && isUserInGroup(user, m_fi.group())))
+       )
+    {
+     if (!showMessageBox)
+        return false;
+     
+     if (KMessageBox::Cancel == KMessageBox::warningContinueCancel(
+         0L,i18n(
+           "<qt>You have specified read access to the user <i>%1</i> for this directory, but "
+           "the user doesn't have the needed write permissions !<br>" 
+           "Do you want to continue nevertheless ?</qt>").arg(user)
+           ,i18n("Warning")
+           ,KStdGuiItem::cont()
+           ,"KSambaPlugin_userHasNoWritePermissionsWarning"))
+         return false;
+    }
+  }    
+  
+  return true;
+}
+    
+bool LinuxPermissionChecker::checkUserReadPermissions(const QString & user, bool showMessageBox) {
+  if (! ((m_fi.permission(QFileInfo::ReadOther)) ||
+         (m_fi.permission(QFileInfo::ReadUser) && user == m_fi.owner()) ||
+         (m_fi.permission(QFileInfo::ReadGroup) && isUserInGroup(user, m_fi.group())))
+     )
+  {     
+     if (!showMessageBox)
+        return false;
+        
+     if (KMessageBox::Cancel == KMessageBox::warningContinueCancel(
+         0L,i18n(
+           "<qt>You have specified read access to the user <i>%1</i> for this directory, but "
+           "the user doesn't have the needed read permissions !<br>" 
+           "Do you want to continue nevertheless ?</qt>").arg(user)
+           ,i18n("Warning")
+           ,KStdGuiItem::cont()
+           ,"KSambaPlugin_userHasNoReadPermissionsWarning"))
+         return false;
+     
+  }
+     
+  return true;     
+}
+
---
+++ advanced/kcm_sambaconf/linuxpermissionchecker.h
@@ -0,0 +1,107 @@
+/***************************************************************************
+    begin                : Tue May 17 2003
+    copyright            : (C) 2003 by Jan Schäfer
+    email                : janschaefer@users.sourceforge.net
+ ***************************************************************************/
+
+/******************************************************************************
+ *                                                                            *
+ *  This file is part of KSambaPlugin.                                        *
+ *                                                                            *
+ *  KSambaPlugin is free software; you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation; either version 2 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  KSambaPlugin is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with KSambaPlugin; if not, write to the Free Software               *
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
+ *                                                                            *
+ ******************************************************************************/
+#ifndef LINUXPERMISSIONCHECKER_H
+#define LINUXPERMISSIONCHECKER_H
+
+#include <qfileinfo.h>
+
+class SambaShare;
+class QWidget;
+
+/**
+A class to check if the Samba permissions specified in the SambaShare fit 
+to the un?x permissions of the directory. E.g. a user specifies a SambaShare as writeable
+for the public, but the guest account has no write permissions for the directory of the
+SambaShare. This will cause a dialog (which can be turned of)  to popup and inform the
+user about the missing rights and ask him to continue nevertheless or to cancel.
+This class works for all un?x systems not only for Linux, but I couldn't call the
+class Un?xPermissionChecker ;-)
+
+@author Jan Schaefer
+*/
+class LinuxPermissionChecker{
+public:
+    
+    LinuxPermissionChecker(SambaShare*,QWidget* parent );
+    ~LinuxPermissionChecker();
+
+    /**
+     * Checks all possible errors that the user could made
+     * @returns <ul>
+     * <li><code>false</code> if the un?x permissions of the directory doesn't fit
+     * to the specified permissions in the Samba share and the user has pushed
+     * the cancel button of one of the dialogs, that informed her.
+     * <li><code>true</code> if either there haven't been any problems with the un?x
+     * permissions, or there have been problems, but the user pressed always the continue button.
+     */
+    bool checkAllPermissions();
+
+    /** 
+     * Checks only the public permissions of the directory. First checks
+     * if the Samba share is specified public and then checks if the guest account
+     * has the appropriate rights.
+     * @return @see #check
+     */
+    bool checkPublicPermissions();
+    
+    /**
+     * Checks all permissions of all users specified in 
+     * the Samba share
+     * @return @see #check
+     */
+    bool checkAllUserPermissions();
+        
+protected:
+    
+    
+    /**
+     * Checks all permissions of the given un?x user
+     * @return @see #check
+     */
+    bool checkUserPermissions(const QString & user);
+
+    /** 
+     * Checks the write permissions of the given un?x user
+     * Does not show a dialog if showMessageBox is false
+     * @return  @see #check
+     */
+    bool checkUserWritePermissions(const QString & user,bool showMessageBox = true);
+    
+    /**
+     * Checks the read permissions of the given un?x user
+     * Does not show a dialog if showMessageBox is false
+     * @return  @see #check
+     */
+    bool checkUserReadPermissions(const QString & user,bool showMessageBox = true);
+        
+
+    QWidget* m_parent;
+    SambaShare* m_sambaShare;
+    QFileInfo m_fi;    
+
+};
+
+#endif

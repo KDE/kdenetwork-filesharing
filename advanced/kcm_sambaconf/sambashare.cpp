403d3cd28834423ff8753fd93f529b1ba442308e
Jan Schäfer <j_schaef@informatik.uni-kl.de>
2003-11-28 15:34:45 +0000

    - Clean indentions
    svn path=/trunk/kdenetwork/filesharing/; revision=270112

--- advanced/kcm_sambaconf/sambashare.cpp
+++ advanced/kcm_sambaconf/sambashare.cpp
@@ -1,68 +1,69 @@
 /***************************************************************************
                           sambashare.cpp  -  description
-                             -------------------
+                            -------------------
     begin                : Mon Jun 12 2002
     copyright            : (C) 2002 by Jan Schäfer
     email                : janschaefer@users.sourceforge.net
- ***************************************************************************/
+***************************************************************************/
 
 /******************************************************************************
- *                                                                            *
- *  This file is part of KSambaPlugin.                                        *
- *                                                                            *
- *  KSambaPlugin is free software; you can redistribute it and/or modify      *
- *  it under the terms of the GNU General Public License as published by      *
- *  the Free Software Foundation; either version 2 of the License, or         *
- *  (at your option) any later version.                                       *
- *                                                                            *
- *  KSambaPlugin is distributed in the hope that it will be useful,           *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
- *  GNU General Public License for more details.                              *
- *                                                                            *
- *  You should have received a copy of the GNU General Public License         *
- *  along with KSambaPlugin; if not, write to the Free Software                     *
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
- *                                                                            *
- ******************************************************************************/
+*                                                                            *
+*  This file is part of KSambaPlugin.                                        *
+*                                                                            *
+*  KSambaPlugin is free software; you can redistribute it and/or modify      *
+*  it under the terms of the GNU General Public License as published by      *
+*  the Free Software Foundation; either version 2 of the License, or         *
+*  (at your option) any later version.                                       *
+*                                                                            *
+*  KSambaPlugin is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU General Public License for more details.                              *
+*                                                                            *
+*  You should have received a copy of the GNU General Public License         *
+*  along with KSambaPlugin; if not, write to the Free Software                     *
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
+*                                                                            *
+******************************************************************************/
 
 #include <kdebug.h>
 #include <qstringlist.h>
 
 #include "sambafile.h"
 #include "sambashare.h"
+#include "common.h"
 
 SambaShare::SambaShare(SambaConfigFile* sambaFile)
-	: QDict<QString>(10,false)
+  : QDict<QString>(10,false)
 {
   _sambaFile = sambaFile;
-	setName("defaults");
+  setName("defaults");
   setAutoDelete(true);
 }
 
 SambaShare::SambaShare(const QString & name, SambaConfigFile* sambaFile)
-	: QDict<QString>(10,false)
+  : QDict<QString>(10,false)
 {
   _sambaFile = sambaFile;
-	setName(name);
+  setName(name);
   setAutoDelete(true);
 }
 
 QString SambaShare::getName() const
 {
-	return _name;
+  return _name;
 }
 
 bool SambaShare::setName(const QString & name, bool testWetherExists)
 {
 
   if ( testWetherExists &&
-       _sambaFile->find(name) &&
-       _sambaFile->find(name) != this)
-     return false;
+      _sambaFile->find(name) &&
+      _sambaFile->find(name) != this)
+    return false;
 
-	_name = name;
- return true;
+  _name = name;
+return true;
 }
 
 bool SambaShare::optionSupported(const QString & name) 
@@ -73,64 +74,64 @@ bool SambaShare::optionSupported(const QString & name)
 }
 
 /**
- * Returns the value of the given parameter
- * if no value is set yet the default value
- * will be returned.
- **/
+* Returns the value of the given parameter
+* if no value is set yet the default value
+* will be returned.
+**/
 QString SambaShare::getValue(const QString & name, bool globalValue, bool defaultValue)
 {
-	QString synonym = getSynonym(name);
+  QString synonym = getSynonym(name);
 
   QString* str = find(synonym);
   QString ret;
 
   if (str) {
-  	 ret = *str;
+    ret = *str;
   }     
   else
   if (globalValue)
-		 ret = getGlobalValue(synonym,defaultValue);
-	else
+    ret = getGlobalValue(synonym,defaultValue);
+  else
   if (defaultValue)
-     ret = getDefaultValue(synonym);
+    ret = getDefaultValue(synonym);
 
 
-	if (name=="writable" || name=="write ok" || name=="writeable")
-  	 ret = SambaFile::textFromBool( ! SambaFile::boolFromText(ret) );
+  if (name=="writable" || name=="write ok" || name=="writeable")
+    ret = textFromBool( ! boolFromText(ret) );
 
   return ret;
 }
 
 bool SambaShare::getBoolValue(const QString & name, bool globalValue, bool defaultValue)
 {
-	return SambaFile::boolFromText(getValue(name,globalValue,defaultValue));
+  return boolFromText(getValue(name,globalValue,defaultValue));
 }
 
 
 QString SambaShare::getGlobalValue(const QString & name, bool defaultValue)
 {
-	if (!_sambaFile)
-  	 return getValue(name,false,defaultValue);
+  if (!_sambaFile)
+    return getValue(name,false,defaultValue);
 
   SambaShare* globals = _sambaFile->find("global");
 
   QString s = globals->getValue(name,false,defaultValue);
 
-	return s;
+  return s;
 }
 
 
 /**
- * Returns the default synonym for the given parameter
- * if no synonym exists the original parameter in lower
- * case is returned
- **/
+* Returns the default synonym for the given parameter
+* if no synonym exists the original parameter in lower
+* case is returned
+**/
 QString SambaShare::getSynonym(const QString & name) const
 {
-	QString lname = name.lower().stripWhiteSpace();
+  QString lname = name.lower().stripWhiteSpace();
 
-	if (lname == "browsable") return "browseable";
-	if (lname == "allow hosts") return "hosts allow";
+  if (lname == "browsable") return "browseable";
+  if (lname == "allow hosts") return "hosts allow";
   if (lname == "auto services") return "preload";
   if (lname == "casesignames") return "case sensitive";
   if (lname == "create mode") return "create mask";
@@ -140,24 +141,28 @@ QString SambaShare::getSynonym(const QString & name) const
   if (lname == "directory") return "path";
   if (lname == "directory mode") return "directory mask";
   if (lname == "exec") return "preexec";
-	if (lname == "group") return "force group";
+  if (lname == "group") return "force group";
   if (lname == "lock dir") return "lock directory";
-	if (lname == "min passwd length") return "min password length";
-	if (lname == "only guest") return "guest only";
-	if (lname == "prefered master") return "preferred master";
-	if (lname == "print ok") return "printable";
-	if (lname == "printcap") return "printcap name";
-	if (lname == "printer") return "printer name";
-	if (lname == "protocol") return "max protocol";
-	if (lname == "public") return "guest ok";
-	if (lname == "writable") return "read only";
-	if (lname == "write ok") return "read only";
-	if (lname == "read only") return "read only";
-	if (lname == "root") return "root directory";
-	if (lname == "root") return "root dir";
-	if (lname == "timestamp logs") return "debug timestamp";
+  if (lname == "min passwd length") return "min password length";
+  if (lname == "only guest") return "guest only";
+  if (lname == "prefered master") return "preferred master";
+  if (lname == "print ok") return "printable";
+  if (lname == "printcap") return "printcap name";
+  if (lname == "printer") return "printer name";
+  if (lname == "protocol") return "max protocol";
+  if (lname == "public") return "guest ok";
+  if (lname == "writable") return "read only";
+  if (lname == "write ok") return "read only";
+  if (lname == "read only") return "read only";
+  if (lname == "root") return "root directory";
+  if (lname == "root") return "root dir";
+  if (lname == "timestamp logs") return "debug timestamp";
   if (lname == "user") return "username";
   if (lname == "users") return "username";
+  if (lname == "idmap uid") return "winbind uid";
+  if (lname == "idmap gid") return "winbind gid";
+  if (lname == "vfs object") return "vfs objects";
+
 
   return lname;
 }
@@ -169,28 +174,28 @@ void SambaShare::setValue(const QString & name, const QString & value, bool glob
   QString newValue = value;
 
   if (newValue.isNull())
-     newValue = "";
+    newValue = "";
   
   if (getName().lower() == "global")
-     globalValue = false;
+    globalValue = false;
 
   if (name=="writable" || name=="write ok" || name=="writeable")
   {
-		synonym = "read only";
-    newValue = SambaFile::textFromBool(!SambaFile::boolFromText(value));
+    synonym = "read only";
+    newValue = textFromBool(!boolFromText(value));
   }
 
   QString global = "";
 
-	if (globalValue && !hasComments(synonym))
+  if (globalValue && !hasComments(synonym))
   {
-		global = getGlobalValue(synonym, false);
+    global = getGlobalValue(synonym, false);
 
-  	if ( newValue.lower() == global.lower() )
+    if ( newValue.lower() == global.lower() )
     {
-			remove(synonym);
+      remove(synonym);
       _optionList.remove(synonym);
-			return;
+      return;
     }
   }
 
@@ -199,42 +204,42 @@ void SambaShare::setValue(const QString & name, const QString & value, bool glob
   // That's because the author of the option has thought about it.
   if (defaultValue && global=="" && !hasComments(synonym))
   {
-  	if ( newValue.stripWhiteSpace().lower() == getDefaultValue(synonym).stripWhiteSpace().lower() )
+    if ( newValue.stripWhiteSpace().lower() == getDefaultValue(synonym).stripWhiteSpace().lower() )
     {
       kdDebug() << getName() << " global: " << global << " remove " << synonym << endl;
-			remove(synonym);
+      remove(synonym);
       _optionList.remove(synonym);
-    	return;
-   	} 
+      return;
+    } 
     
   }
 
   if (!find(synonym))
   {
-     _optionList.append(synonym);
+    _optionList.append(synonym);
   }
-     
-	replace(synonym,new QString(newValue));
+    
+  replace(synonym,new QString(newValue));
 }
 
 void SambaShare::setValue(const QString & name, bool value, bool globalValue, bool defaultValue)
 {
-	setValue(name,SambaFile::textFromBool(value),globalValue, defaultValue);
+  setValue(name,textFromBool(value),globalValue, defaultValue);
 }
 
 void SambaShare::setValue(const QString & name, int value, bool globalValue, bool defaultValue)
 {
-	setValue(name,QString::number(value),globalValue, defaultValue);
+  setValue(name,QString::number(value),globalValue, defaultValue);
 }
 
 /**
- * Returns the default value of the parameter
- **/
+* Returns the default value of the parameter
+**/
 QString SambaShare::getDefaultValue(const QString & name)
 {
   QString defaultValue = _sambaFile->getDefaultValue(name);
   if (defaultValue.isNull())
-     defaultValue = "";
+    defaultValue = "";
 
   return defaultValue;
 }
@@ -242,17 +247,17 @@ QString SambaShare::getDefaultValue(const QString & name)
 bool SambaShare::getDefaultBoolValue(const QString & name)
 {
 
-	return SambaFile::boolFromText(getDefaultValue(name));
+  return boolFromText(getDefaultValue(name));
 }
 
 /**
- * Sets the comments for the passed option
- **/
+* Sets the comments for the passed option
+**/
 void SambaShare::setComments(const QString & name, const QStringList & commentList)
 {
   // Only add inempty lists
   if (commentList.empty())
-     return;
+    return;
 
   QString synonym = getSynonym(name);
 
@@ -260,16 +265,16 @@ void SambaShare::setComments(const QString & name, const QStringList & commentLi
 }
 
 /**
- * Returns the comments of the passed option
- **/
+* Returns the comments of the passed option
+**/
 QStringList SambaShare::getComments(const QString & name)
 {
   QStringList* list = _commentList.find(getSynonym(name));
 
   if (list)
-     return QStringList(*list);
+    return QStringList(*list);
   else
-     return QStringList();
+    return QStringList();
 }
 
 
@@ -279,18 +284,18 @@ bool SambaShare::hasComments(const QString & name)
 }
 
 /**
- * Returns the comments of the share
- * e.g. the text above the [...] section
- **/
+* Returns the comments of the share
+* e.g. the text above the [...] section
+**/
 QStringList SambaShare::getComments()
 {
   return _comments;
 }
 
 /**
- * Sets the comments for the share
- * e.g. the text above the [...] section
- **/
+* Sets the comments for the share
+* e.g. the text above the [...] section
+**/
 void SambaShare::setComments(const QStringList & commentList)
 {
   _comments = commentList;
@@ -302,28 +307,28 @@ QStringList SambaShare::getOptionList()
 }
 
 /**
- * Returns true if this share is a printer
- **/
+* Returns true if this share is a printer
+**/
 bool SambaShare::isPrinter()
 {
-	QString* str = find("printable");
+  QString* str = find("printable");
 
   if (!str)
-  	  str = find("print ok");
+      str = find("print ok");
 
-	return str!=0;
+  return str!=0;
 }
 
 /**
- * Returns true if the share name is
- * global, printers or homes
- **/
+* Returns true if the share name is
+* global, printers or homes
+**/
 bool SambaShare::isSpecialSection()
 {
   if ( _name.lower() == "global" ||
-       _name.lower() == "printers" ||
-       _name.lower() == "homes" )
-     return true;
+      _name.lower() == "printers" ||
+      _name.lower() == "homes" )
+    return true;
   else
-     return false;
+    return false;
 }

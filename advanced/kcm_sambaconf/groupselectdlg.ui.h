e5ea31bb3e87b49bf3ae99825ad54fb4157a81f0
Jan Schäfer <j_schaef@informatik.uni-kl.de>
2003-11-28 15:54:07 +0000

    first commit
    svn path=/trunk/kdenetwork/filesharing/; revision=270117

---
+++ advanced/kcm_sambaconf/groupselectdlg.ui.h
@@ -0,0 +1,44 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename functions or slots use
+** Qt Designer which will update this file, preserving your code. Create an
+** init() function in place of a constructor, and a destroy() function in
+** place of a destructor.
+*****************************************************************************/
+
+
+void GroupSelectDlg::init(const QStringList & specifiedGroups)
+{
+  QStringList unixGroups = getUnixGroups();
+
+  for (QStringList::Iterator it = unixGroups.begin(); it != unixGroups.end(); ++it)
+  {
+      if ( ! specifiedGroups.contains(*it))
+	  new QListViewItem(groupListView, *it, QString::number(getGroupGID(*it)));
+  }
+}
+
+void GroupSelectDlg::accept()
+{
+  QListViewItemIterator it( groupListView, QListViewItemIterator::Selected );
+
+  for ( ; it.current(); ++it ) {
+      selectedGroups << it.current()->text(0);
+  }	
+  
+  access = accessBtnGrp->selectedId();
+  
+  if (unixRadio->isChecked())
+      groupKind = "+";
+  else
+      if (nisRadio->isChecked())
+	  groupKind = "&";
+  else
+      if (bothRadio->isChecked())
+	  groupKind = "@";
+  
+  QDialog::accept();
+}
+
+
---
+++ advanced/kcm_sambaconf/userselectdlg.ui.h
@@ -0,0 +1,37 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename functions or slots use
+** Qt Designer which will update this file, preserving your code. Create an
+** init() function in place of a constructor, and a destroy() function in
+** place of a destructor.
+*****************************************************************************/
+
+
+void UserSelectDlg::init(const QStringList & specifiedUsers, SambaShare* share)
+{
+  SmbPasswdFile passwd( KURL(share->getValue("smb passwd file",true,true)) );
+  SambaUserList sambaList = passwd.getSambaUserList();
+
+  for (SambaUser * user = sambaList.first(); user; user = sambaList.next() )
+  {
+    if (! specifiedUsers.contains(user->name))
+        new QListViewItem(userListView, user->name, QString::number(user->uid), QString::number(user->gid));	
+  }
+
+}
+
+
+void UserSelectDlg::accept()
+{
+  QListViewItemIterator it( userListView, QListViewItemIterator::Selected );
+
+  for ( ; it.current(); ++it ) {
+      selectedUsers << it.current()->text(0);
+  }	
+  
+  access = accessBtnGrp->selectedId();
+  
+  QDialog::accept();
+
+}

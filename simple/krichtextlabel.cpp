7d06077d17f033d512c96e11c1f25b3e41388c5d
Laurent Montel <montel@kde.org>
2009-01-02 12:25:36 +0000

    Qt3support--
    svn path=/trunk/KDE/kdenetwork/filesharing/; revision=904459

--- simple/krichtextlabel.cpp
+++ simple/krichtextlabel.cpp
@@ -17,6 +17,7 @@
 */
 
 #include "krichtextlabel.h"
+#include <QTextDocument>
 
 
 #include <q3stylesheet.h>
@@ -70,20 +71,22 @@ QSize KRichTextLabel::minimumSizeHint() const
   QString qt_text = qrichtextify( text() );
   int pref_width = 0;
   int pref_height = 0;
-  Q3SimpleRichText rt(qt_text, font());
+  QTextDocument rt;
+  rt.setHtml( qt_text );
+
   pref_width = m_defaultWidth;
-  rt.setWidth(pref_width);
-  int used_width = rt.widthUsed();
+  rt.setTextWidth(pref_width);
+  int used_width = rt.idealWidth();
   if (used_width <= pref_width)
   {
     while(true)
     {
       int new_width = (used_width * 9) / 10;
-      rt.setWidth(new_width);
-      int new_height = rt.height();
+      rt.setTextWidth(new_width);
+      int new_height = rt.size().height();
       if (new_height > pref_height)
         break;
-      used_width = rt.widthUsed();
+      used_width = rt.idealWidth();
       if (used_width > new_width)
         break;
     }
@@ -97,7 +100,7 @@ QSize KRichTextLabel::minimumSizeHint() const
       pref_width = used_width;
   }
 
-  return QSize(pref_width, rt.height());
+  return QSize(pref_width, rt.size().height());
 }
 
 QSize KRichTextLabel::sizeHint() const
